39e5a4b1b358609c402888a6dfa146d5dea1de8f
JorgeDS <jrivera@piensads.com>
2024-10-11 17:04:52 -0600

    Adding constants and api functions

---
+++ src/services/api.js
@@ -0,0 +1,85 @@
+import { API_URL } from '@Configs/constants.js';
+import { getParams } from '@Utils/functions.js';
+
+export async function getData(resource, data) {
+  let endpoint = `${API_URL}/${resource}.php`;
+  if (data !== undefined) {
+    endpoint += `?${getParams}`;
+  }
+  try {
+    const Response = await axios.get(endpoint).then((res) => res.json());
+    if (Response !== undefined) {
+      return Response.data;
+    }
+  } catch (er) {
+    if (er instanceof Error) {
+      console.log(er);
+    }
+  }
+}
+
+export async function sendData(resource, data) {
+  let endpoint = `${API_URL}/${resource}.php`;
+  try {
+    const Response = await axios
+      .post(endpoint, {
+        method: 'post',
+        data: JSON.stringify(data),
+        Headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res.json());
+    if (Response !== undefined) {
+      return Response.data;
+    }
+  } catch (er) {
+    if (er instanceof Error) {
+      console.log(er);
+    }
+  }
+}
+
+export async function updateData(resource, data) {
+  let endpoint = `${API_URL}/${resource}.php`;
+  try {
+    const Response = await axios
+      .put(endpoint, {
+        method: 'put',
+        data: JSON.stringify(data),
+        Headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res.json());
+    if (Response !== undefined) {
+      return Response.data;
+    }
+  } catch (er) {
+    if (er instanceof Error) {
+      console.log(er);
+    }
+  }
+}
+
+export async function deleteData(resource, data) {
+  let endpoint = `${API_URL}/${resource}.php`;
+  try {
+    const Response = await axios
+      .delete(endpoint, {
+        method: 'delete',
+        data: JSON.stringify(data),
+        Headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((res) => res.json());
+    if (Response !== undefined) {
+      return Response.data;
+    }
+  } catch (er) {
+    if (er instanceof Error) {
+      console.log(er);
+    }
+  }
+}
