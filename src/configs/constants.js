2ba853808864233dfbb02241fb0ca2e5924f1eec
JorgeDS <jrivera@piensads.com>
2024-10-14 13:12:34 -0600

    Adding constants

--- src/configs/constants.js
+++ src/configs/constants.js
@@ -1,1 +1,5 @@
 export const API_URL = import.meta.env.API_URL;
+export const APP_NAME = import.meta.env.APP_NAME;
+export const APP_VERSION = import.meta.env.APP_VERSION;
+export const APP_DESCRIPTION = import.meta.env.APP_DESCRIPTION;
+export const APP_URL = import.meta.env.APP_URL;
