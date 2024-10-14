449b8d816a78c4be08ab218319bbfbca4b0cf89d
JorgeDS <jrivera@piensads.com>
2024-10-14 13:12:17 -0600

    Adding Variables to types

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly APP_SHORT_VERSION: string;
   readonly APP_VERSION: string;
   readonly APP_KEY: string;
+  readonly APP_DESCRIPTION: string;
+  readonly APP_URL: string;
 }
 
 interface ImportMeta {
