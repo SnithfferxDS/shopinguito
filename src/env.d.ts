4f0e4ed62e3e120acfd50105ce018e105151bc9e
JorgeDS <jrivera@piensads.com>
2024-10-16 10:51:26 -0600

    Updating configs

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,12 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 interface ImportMetaEnv {
   readonly API_URL: string;
+  readonly API_TOKEN: string;
+  readonly API_KEY: string;
+  readonly APP_URL: string;
   readonly APP_NAME: string;
   readonly APP_SHORT_VERSION: string;
   readonly APP_VERSION: string;
-  readonly APP_KEY: string;
   readonly APP_DESCRIPTION: string;
-  readonly APP_URL: string;
+  readonly APP_KEY: string;
 }
 
 interface ImportMeta {
